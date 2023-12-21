f287d8d11b5f886403cedc0241457ef80171fc24
HONGXIANG <152053752+XavierWangHX@users.noreply.github.com>
2023-12-21 20:40:15 +0800

    Update oss.go

--- config/oss.go
+++ config/oss.go
@@ -6,7 +6,7 @@ const (
 	// OSSEndpoint : oss endpoint
 	OSSEndpoint = "oss-cn-chengdu.aliyuncs.com"
 	// OSSAccesskeyID : oss访问key
-	OSSAccesskeyID = "LTAI5tH7gfwTh6wfeSjLTsTq"
+	OSSAccesskeyID = ""
 	// OSSAccessKeySecret : oss访问key secret
-	OSSAccessKeySecret = "1qtZ1jlT7QgIItElUFlnvL7OG53JMQ"
+	OSSAccessKeySecret = ""
 )
